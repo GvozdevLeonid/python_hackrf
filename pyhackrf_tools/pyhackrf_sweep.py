4d32f8a4d02683cc4d6b13a8959cd7d68714e617
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2023-10-01 22:13:50 +0200

    added LICENSE

--- pyhackrf_tools/pyhackrf_sweep.py
+++ pyhackrf_tools/pyhackrf_sweep.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2023 GvozdevLeonid
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from python_hackrf import pyhackrf
 try:
     from scipy.fft import fft
@@ -216,10 +238,10 @@ def pyhackrf_sweep(frequencies: list = [0, 6000], lna_gain: int = 16, vga_gain: 
         fftSize += 1
     
     # setup nessesary arrays
-    # window = np.hanning(fftSize)
-    window = np.empty(shape=(fftSize,), dtype=np.float32)
-    for i in range(fftSize):
-        window[i] = 0.5 * (1.0 - np.cos(2 * np.pi * i / (fftSize - 1)))
+    window = np.hanning(fftSize)
+    # window = np.empty(shape=(fftSize,), dtype=np.float32)
+    # for i in range(fftSize):
+    #     window[i] = 0.5 * (1.0 - np.cos(2 * np.pi * i / (fftSize - 1)))
     
     fftwIn = np.empty(shape=(fftSize,), dtype=np.complex64)
     pwr = np.empty(shape=(fftSize,), dtype=np.float32)
