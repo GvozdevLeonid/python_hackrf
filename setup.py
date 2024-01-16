90fdfc6448bb0c19adacc256c772ff5d81d7f044
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2024-01-16 12:46:48 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -45,8 +45,6 @@ source_files.append('python_hackrf/pylibhackrf/hackrf.c')
 
 setup(
     name='python_hackrf',
-    author='Leonid Gvozdev',
-    author_email='leo.gvozdev.dev@gmail.com',
     cmdclass={'build_ext': build_ext},
     install_requires=INSTALL_REQUIRES,
     setup_requires=SETUP_REQUIRES,
