731b37000a88de38712985557b77ee4f4c5c79bf
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2025-01-15 15:52:21 +0100

    updated

--- python_hackrf/pylibhackrf/pyhackrf.pyx
+++ python_hackrf/pylibhackrf/pyhackrf.pyx
@@ -96,7 +96,7 @@ class py_operacake_ports(IntEnum):
 cdef int __rx_callback(chackrf.hackrf_transfer* transfer) noexcept with gil:
     global global_callbacks
 
-    np_buffer = np.frombuffer(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
+    np_buffer = np.asarray(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
     if global_callbacks[<size_t> transfer.device]['__rx_callback'] is not None:
         result = global_callbacks[<size_t> transfer.device]['__rx_callback'](global_callbacks[<size_t> transfer.device]['device'], np_buffer, transfer.buffer_length, transfer.valid_length)
         return result
@@ -112,6 +112,8 @@ cdef int __tx_callback(chackrf.hackrf_transfer* transfer) noexcept with gil:
     if global_callbacks[<size_t> transfer.device]['__tx_callback'] is not None:
         result = global_callbacks[<size_t> transfer.device]['__tx_callback'](global_callbacks[<size_t> transfer.device]['device'], np_buffer, transfer.buffer_length, valid_length)
         transfer.valid_length = valid_length.value
+        for i in range(valid_length.value):
+            transfer.buffer[i] = <uint8_t> np_buffer[i]
         return result
     return -1
 
@@ -120,7 +122,7 @@ cdef int __tx_callback(chackrf.hackrf_transfer* transfer) noexcept with gil:
 cdef int __sweep_callback(chackrf.hackrf_transfer* transfer) noexcept with gil:
     global global_callbacks
 
-    np_buffer = np.frombuffer(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
+    np_buffer = np.asarray(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
     if global_callbacks[<size_t> transfer.device]['__sweep_callback'] is not None:
         result = global_callbacks[<size_t> transfer.device]['__sweep_callback'](global_callbacks[<size_t> transfer.device]['device'], np_buffer, transfer.buffer_length, transfer.valid_length)
         return result
@@ -131,7 +133,7 @@ cdef int __sweep_callback(chackrf.hackrf_transfer* transfer) noexcept with gil:
 cdef void __tx_complete_callback(chackrf.hackrf_transfer* transfer, int success) noexcept with gil:
     global global_callbacks
 
-    np_buffer = np.frombuffer(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
+    np_buffer = np.asarray(<uint8_t[:transfer.buffer_length]> transfer.buffer, dtype=np.int8)  # type: ignore
     if global_callbacks[<size_t> transfer.device]['__tx_complete_callback'] is not None:
         global_callbacks[<size_t> transfer.device]['__tx_complete_callback'](global_callbacks[<size_t> transfer.device]['device'], np_buffer, transfer.buffer_length, transfer.valid_length, success)
 
