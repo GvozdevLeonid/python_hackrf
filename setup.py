260b079c826a026ee9d18038b3a53e14f5a52624
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2025-02-23 12:01:33 +0100

    updated

--- setup.py
+++ setup.py
@@ -52,6 +52,9 @@ if PLATFORM != 'android':
     environ['CFLAGS'] = f'{cflags} {new_cflags}'.strip()
     environ['LDFLAGS'] = f'{ldflags} {new_ldflags}'.strip()
 
+else:
+    LIBHACKRF_FILES += ['python_hackrf/pylibhackrf/hackrf_android.c']
+
 class CustomBuildExt(build_ext):
     def run(self):
         compile_env = {'ANDROID': PLATFORM == 'android'}
