06539e4929ddd53dc3aaad7b907101bfa70774ba
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2025-06-10 17:17:36 +0200

    cleaned up the code. bump to 1.4.0

--- android/python_hackrf/__init__.py
+++ android/python_hackrf/__init__.py
@@ -1,28 +1,34 @@
+# ruff: noqa: RUF012
 import os
+from typing import Any
 
 from pythonforandroid.archs import Arch
 from pythonforandroid.recipe import (
-    CythonRecipe,
+    PyProjectRecipe,
     Recipe,
 )
 
 
-class PythonHackrfRecipe(CythonRecipe):
+class PythonHackrfRecipe(PyProjectRecipe):
     url = 'https://github.com/GvozdevLeonid/python_hackrf/releases/download/v.{version}/python_hackrf-{version}.tar.gz'
-    depends = ('python3', 'setuptools', 'numpy', 'pyjnius', 'libhackrf')
+    depends = ['python3', 'setuptools', 'numpy', 'pyjnius', 'libhackrf']
+    hostpython_prerequisites = ['Cython>=3.1.0,<3.2']
     site_packages_name = 'python_hackrf'
     name = 'python_hackrf'
-    version = '1.3.2'
+    version = '1.4.0'
 
-    def get_recipe_env(self, arch: Arch) -> dict:
-        env = super().get_recipe_env(arch)
+    def get_recipe_env(self, arch: Arch, **kwargs) -> dict[str, Any]:
+        env: dict[str, Any] = super().get_recipe_env(arch, **kwargs)
 
         libhackrf_recipe = Recipe.get_recipe('libhackrf', arch)
-
         libhackrf_h_dir = os.path.join(libhackrf_recipe.get_build_dir(arch), 'host', 'libhackrf', 'src')
 
-        env['LDFLAGS'] += f' -L{self.ctx.get_libs_dir(arch.arch)}'
-        env['CFLAGS'] += f' -I{libhackrf_h_dir}'
+        env['LDFLAGS'] = env['LDFLAGS'] + f' -L{self.ctx.get_libs_dir(arch.arch)} -lhackrf'
+        env['CFLAGS'] = env['CFLAGS'] + f' -I{libhackrf_h_dir}'
+
+        env['PYTHON_BLADERF_LIBBLADERF_H_PATH'] = libhackrf_h_dir
+        env['LDSHARED'] = env['CC'] + ' -shared'
+        env['LIBLINK'] = 'NOTNONE'
 
         return env
 
