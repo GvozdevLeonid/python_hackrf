f6efc70f37ab91871e3cba624a7d8893a0c02c1d
Gvozdev Leonid <gvozdev-leonid-95@yandex.ru>
2025-11-09 18:57:09 +0100

    fixed typo

--- python_hackrf/pyhackrf_tools/pyhackrf_scan.pyi
+++ python_hackrf/pyhackrf_tools/pyhackrf_scan.pyi
@@ -6,7 +6,7 @@ def stop_all() -> None:
 def stop_sdr(serialno: str) -> None:
     ...
 
-def pyhackrf_sweep(frequencies: list[int], samples_per_scan: int, queue: object, sample_rate: int = 20_000_000, baseband_filter_bandwidth: int | None = None,
-                   lna_gain: int = 16, vga_gain: int = 20, amp_enable: bool = False, antenna_enable: bool = False, serial_number: str | None = None,
-                   print_to_console: bool = True) -> None:
+def pyhackrf_scan(frequencies: list[int], samples_per_scan: int, queue: object, sample_rate: int = 20_000_000, baseband_filter_bandwidth: int | None = None,
+                  lna_gain: int = 16, vga_gain: int = 20, amp_enable: bool = False, antenna_enable: bool = False, serial_number: str | None = None,
+                  print_to_console: bool = True) -> None:
     ...
